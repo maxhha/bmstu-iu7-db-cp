bad6ae062d92ecd4b7b3bab54feecb41c78a896b
maxhha <devmaxhha@gmail.com>
2022-04-01 18:35:53 +0300

    Add mock-redis

--- go.mod
+++ go.mod
@@ -20,7 +20,10 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -55,7 +58,7 @@ require (
 	github.com/urfave/cli/v2 v2.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/tools v0.1.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- services/mock_notifier/server.go
+++ services/mock_notifier/server.go
@@ -3,35 +3,55 @@ package main
 import (
 	"auction-back/grpc/notifier"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 
+	"github.com/go-redis/redis/v8"
 	"google.golang.org/grpc"
 )
 
 type NotifierServer struct {
 	notifier.UnimplementedNotifierServer
+	rdb *redis.Client
 }
 
-func New() *NotifierServer {
-	return &NotifierServer{}
+func New(rdb *redis.Client) *NotifierServer {
+	return &NotifierServer{rdb: rdb}
 }
 
 func (s *NotifierServer) Send(ctx context.Context, input *notifier.SendInput) (*notifier.SendResult, error) {
-	fmt.Printf("\nreceivers: %v\naction: %v\ndata: %v\n", input.Receivers, input.Action, input.Data)
-	// TODO: send to redis
+	key := fmt.Sprintf("send-%s-%s", input.Action, strings.Join(input.Receivers, ","))
+	fmt.Printf("\n%s\nreceivers: %v\naction: %v\ndata: %v\n", key, input.Receivers, input.Action, input.Data)
+
+	data, err := json.Marshal(input.Data)
+	if err != nil {
+		return &notifier.SendResult{Status: fmt.Sprintf("json: %v", err)}, nil
+	}
+
+	if err := s.rdb.Set(ctx, key, data, 0).Err(); err != nil {
+		return &notifier.SendResult{Status: fmt.Sprintf("rdb set: %v", err)}, nil
+	}
+
 	return &notifier.SendResult{Status: "OK"}, nil
 }
 
 func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		log.Fatalln("PORT is not defiend in environment variables!")
 	}
 
-	host, _ := os.LookupEnv("HOST")
+	host := os.Getenv("HOST")
 	address := fmt.Sprintf("%s:%s", host, port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
@@ -39,7 +59,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	notifier.RegisterNotifierServer(grpcServer, New())
+	notifier.RegisterNotifierServer(grpcServer, New(rdb))
 
 	fmt.Printf("Start listen on %s\n", address)
 	grpcServer.Serve(lis)
